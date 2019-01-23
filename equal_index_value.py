396772c936bf4a9c25e3ce140f84af7c18486afe
banumathimurugan <banufrisbee@gmail.com>
2019-01-23 11:57:25 +0530

    Create equal_index_value.py

---
+++ equal_index_value.py
@@ -0,0 +1,14 @@
+j=int(input())
+l=input().split()
+b=[]
+s=len(1)
+for su in range(0,s):
+  p=int(1[su])
+  if(p==su):
+    a.append(su)
+if(len(a)!=0):
+  s=' '.join(map(str,b))
+  print(s)
+else:
+  print("-1")
+  
